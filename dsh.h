37e9343bab9397eb3fa41ff8b350ec6cc1271cba
akirner1 <akirner@pugetsound.edu>
2024-02-16 07:01:48 +0000

    updated documention to warn users that getEachWord destroys the inputted string

--- dsh.h
+++ dsh.h
@@ -4,7 +4,7 @@
 
 // TODO: Function declarations go below
 /**
- * @param line a string to be processed
+ * @param line a string to be processed. This string will be destroyed upon execution.
  * @param lineSize the size of line (number of characters)
  * @returns a pointer to an array of strings, with each string being a single word from line 
  * Will stop processing after lineSize-1 characters have been processed (since we assume the last char is "\0"), or a "\0" is read)
